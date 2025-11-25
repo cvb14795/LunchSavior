e699b60f46e2416b65e48697dbb8ec331ddc2188
cvb14795 <cvb14795@gmail.com>
2025-11-26 00:01:35 +0800

    Initial setup: Spring Boot project with PostgreSQL configuration

---
+++ src/main/java/com/example/LunchSavior/LunchSaviorApplication.java
@@ -0,0 +1,13 @@
+package com.example.LunchSavior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchSaviorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchSaviorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LunchSavior/LunchSaviorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LunchSavior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchSaviorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
